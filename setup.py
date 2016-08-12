f9d6a627568ba93e5bba5cd56da707f5a975a16e
Christopher Bruns <cmbruns@rotatingpenguin.com>
2016-08-12 15:48:20 -0400

    Rough pass at populating setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,26 @@
+#!/bin/env python
+
 '''
 Created on Aug 9, 2016
 
-@author: brunsc
+@author: Christopher Bruns
 '''
+
+from setuptools import setup
+
+# Load module version from ovr/version.py
+exec(open('src/ktx/version.py').read())
+
+setup(
+    name = "pyktx",
+    version = __version__,
+    author = "Christopher Bruns",
+    author_email = "cmbruns@rotatingpenguin.com",
+    description = "Pure python tools for reading and writing KTX format OpenGL image texture files",
+    url = "https://github.com/cmbruns/pyktx",
+    download_url = "https://github.com/cmbruns/pyktx/tarball/" + __version__,
+    package_dir = {'': 'src'},
+    packages = ['ktx',],
+    keywords = "opengl ktx texture file format",
+    classifiers = [],
+)
