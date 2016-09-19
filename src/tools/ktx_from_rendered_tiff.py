4b74605fe5ae1b4523a1f85fa96c03e5abe5903e
Christopher Bruns <cmbruns@rotatingpenguin.com>
2016-09-19 12:19:59 -0400

    Populate octree metatdata in small-memory ktx creation

--- src/tools/ktx_from_rendered_tiff.py
+++ src/tools/ktx_from_rendered_tiff.py
@@ -7,11 +7,15 @@ Created on Sep 15, 2016
 import os
 import glob
 import io
+import re
+import math
+import datetime
 
 import numpy
 from libtiff import TIFF
 
 from small_memory_histogram import histogram_tiff_file
+import ktx
 from ktx import KtxHeader
 from ktx.util import mipmap_shapes, _assort_subvoxels, _filter_assorted_array,\
     interleave_channel_arrays
@@ -22,6 +26,7 @@ class RenderedMouseLightOctree(object):
     
     def __init__(self, folder):
         self.folder = folder
+        # Parse the origin and voxel size from file "transform.txt" in the root folder
         self.transform = dict()
         with io.open(os.path.join(folder, "transform.txt"), 'r') as transform_file:
             for line in transform_file:
@@ -29,6 +34,19 @@ class RenderedMouseLightOctree(object):
                 if len(fields) != 2:
                     continue
                 self.transform[fields[0].strip()] = fields[1].strip()
+        # Convert nanometers to micrometers
+        umFromNm = 1.0/1000.0
+        self.origin_um = umFromNm * numpy.array([float(self.transform[key]) for key in ['ox', 'oy', 'oz']], dtype='float64')
+        self.voxel_um = umFromNm * numpy.array([float(self.transform[key]) for key in ['sx', 'sy', 'sz']], dtype='float64')
+        self.number_of_levels = int(self.transform['nl'])
+        self.specimen_id = os.path.split(folder)[-1]
+        # Get base image size, so we can convert voxel size into total volume size
+        temp_block = RenderedTiffBlock(folder, self, [])
+        temp_block._populate_size_and_histograms()
+        # Rearrange image size from zyx to xyz
+        self.volume_voxels = numpy.array([temp_block.zyx_size[i] for i in [2, 1, 0]], dtype='uint32')
+        # Compute total volume size in micrometers
+        self.volume_um = self.voxel_um * self.volume_voxels
 
     def iter_blocks(self, max_level=None, folder=None):
         "Walk through rendered blocks, starting at folder folder, up to max_level steps deeper"
@@ -39,7 +57,13 @@ class RenderedMouseLightOctree(object):
             return
         if not os.path.isdir(folder):
             return
-        yield RenderedTiffBlock(folder)
+        octree_path0 = os.path.relpath(folder, self.folder)
+        octree_path0 = octree_path0.split('/')
+        octree_path = []
+        for level in octree_path0:
+            if re.match(r'[1-8]', level):
+                octree_path.append(int(level) - 1)
+        yield RenderedTiffBlock(folder, self, octree_path)
         if level == max_level:
             return
         for subfolder in [str(i) for i in range(1, 9)]:
@@ -51,10 +75,16 @@ class RenderedMouseLightOctree(object):
 class RenderedTiffBlock(object):
     "RenderedBlock represents all channels of one rendered Mouse Light volume image block"
     
-    def __init__(self, folder):
+    def __init__(self, folder, octree_root, octree_path, mipmap_filter='arthur', downsample_xy=False, downsample_intensity=False):
         "Folder contains one or more 'default.0.tif', 'default.1.tif', etc. channel files"
+        self.folder = folder
+        self.octree_root = octree_root
+        self.octree_path = octree_path
         self.channel_files = glob.glob(os.path.join(folder, "default.*.tif"))
         self.zyx_size = None
+        self.downsample_xy = downsample_xy
+        self.downsample_intensity = downsample_intensity
+        self.mipmap_filter = mipmap_filter
 
     def _populate_size_and_histograms(self):
         """
@@ -79,7 +109,73 @@ class RenderedTiffBlock(object):
                 shape=self.zyx_size, 
                 dtype=self.dtype, 
                 channel_count=len(self.channels))
+        self._populate_octree_metadata()
         
+    def _populate_octree_metadata(self):
+        kh = self.ktx_header
+        kh["distance_units"] = "micrometers"
+        kh['multiscale_level_id'] = len(self.octree_path)
+        kh['multiscale_total_levels'] = self.octree_root.number_of_levels
+        kh['octree_path'] = "/".join(self.octree_path)
+        assert kh['multiscale_level_id'] < kh['multiscale_total_levels']
+        # Walk octree path to compute geometric parameters. Because someone
+        # decided that having a separate transform.txt in each folder was
+        # "inefficient" and implemented it that way in production one day as a 
+        # fait accompli.
+        # Initialize with top-level origin and size
+        self.origin_um = numpy.array(self.octree_root.origin_um, copy=True)
+        self.volume_um = numpy.array(self.octree_root.origin_um, copy=True)
+        for level in self.octree_path: # range 0-7
+            self.volume_um *= 0.5 # Every deeper level is half the size of the lower level
+            # Shift origin if this is a right/bottom/far sub-block
+            bigZ = level > 4 # 4,5,6,7
+            bigX = level % 2 > 0 # 1,3,5,7
+            bigY = level in [2,3,6,7] # 2,3,6,7
+            if bigZ: # if Z is large, shift Z origin
+                self.origin_um[2] += self.volume_um[2]
+            if bigY:
+                self.origin_um[1] += self.volume_um[1]
+            if bigX:
+                self.origin_um[0] += self.volume_um[0]
+        # Compute linear transform from texture coordinates to microscope stage coordinates
+        ox, oy, oz = self.origin_um
+        sx, sy, sz = self.volume_um
+        xform = numpy.array([
+                [sx, 0, 0, ox],
+                [0, sy, 0, oy],
+                [0, 0, sz, oz],
+                [0, 0, 0, 1],], dtype='float64')
+        kh["xyz_from_texcoord_xform"] = xform
+        center = numpy.array( (ox + 0.5*sx, oy + 0.5*sy, oz + 0.5*sz,), )
+        radius = math.sqrt(sx*sx + sy*sy + sz*sz)/16.0
+        kh['bounding_sphere_center'] = center
+        kh['bounding_sphere_radius'] = radius        
+        # Nominal resolution
+        resX = sx / kh.pixel_width
+        resY = sy / kh.pixel_height
+        resZ = sz / kh.pixel_depth
+        rms = math.sqrt(numpy.mean(numpy.square([resX, resY, resZ],)))
+        kh['nominal_resolution'] = rms
+        # Specimen ID
+        kh['specimen_id'] = self.octree_root.specimen_id
+        # Relation to parent tile/block
+        kh['mipmap_filter'] = self.mipmap_filter
+        relations = list()
+        if self.downsample_xy:
+            relations.append("downsampled 2X in X & Y")
+        if self.downsample_intensity:
+            relations.append("rescaled intensity to 8 bits")
+        if len(relations) == 0:
+            relations.append("unchanged")
+        kh['relation_to_parent'] = ";".join(relations)
+        # TODO: Per channel statistics
+        kh['ktx_file_creation_date'] = datetime.datetime.now()
+        # print (kh['ktx_file_creation_date'])
+        import __main__ #@UnresolvedImport
+        kh['ktx_file_creation_program'] = __main__.__file__
+        # print (kh['ktx_file_creation_program'])
+        kh['pyktx_version'] = ktx.__version__
+
     def _process_tiff_slice(self, z_index, channel_slices, output_stream):
         # Interleave individual color channels into one multicolor slice
         zslice0 = interleave_channel_arrays(channel_slices)
