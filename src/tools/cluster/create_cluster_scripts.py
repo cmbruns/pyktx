7d0ad8b7d0385919959c565ada20e632948c02c5
Christopher Bruns <cmbruns@rotatingpenguin.com>
2016-10-18 16:53:33 -0400

    Enhance cluster conversion for second test brain

--- src/tools/cluster/create_cluster_scripts.py
+++ src/tools/cluster/create_cluster_scripts.py
@@ -10,16 +10,17 @@ echo $PYTHONPATH
 /misc/local/python3/bin/python3.5 $KTXSRC/tools/convert_subtree.py "%s" "%s" "%s" %d
 """
 
-input_root = "/nobackup2/mouselight/2016-04-04b"
+# input_root = "/nobackup2/mouselight/2016-04-04b"
+input_root = "/nobackup2/mouselight/2016-07-18b"
 output_root = "/nobackup2/mouselight/brunsc/ktxtest"
-num_levels = 3
+subtree_depth = 3
 
-def recurse_octree(folder, level):
+def recurse_octree(folder, level, specimen_name):
     if not os.path.exists(folder):
         return
     # if level > 4:
     #     return
-    if level % num_levels == 2: # just levels 2 and 5 (we'll fill in level 1 manually)
+    if level == 1 or level % subtree_depth == 2: # just levels 2 and 5 (we'll fill in level 1 manually)
         print (folder)
         if level == 1:
             subtree0 = []
@@ -27,18 +28,24 @@ def recurse_octree(folder, level):
             subtree0 = folder.split('/')[-(level-1):]
         subtree = "/".join(subtree0)
         print (subtree)
-        script = script_base % (input_root, output_root, subtree, num_levels)
+        script = script_base % (input_root, output_root, subtree, subtree_depth)
         # print (script)
         script_name = "subtree%s.sh" % "".join(subtree0)
         print (script_name)
         if True:
-            f = open('jobscripts/%s' % script_name, 'w')
+            f = open('jobscripts_%s/%s' % (specimen_name, script_name), 'w')
             f.write(script)
             f.close()
     # return
     for i0 in range(8):
         subfolder = folder + '/' + str(i0+1)
-        recurse_octree(subfolder, level + 1)
+        recurse_octree(subfolder, level + 1, specimen_name)
 
-recurse_octree(input_root, 1)
+specimen_name = input_root.split('/')[-1]
+print (specimen_name)
+try:
+    os.makedirs("jobscripts_%s" % specimen_name)
+except:
+    pass
+recurse_octree(input_root, 1, specimen_name)
 
--- src/tools/cluster/submit.py
+++ src/tools/cluster/submit.py
@@ -4,13 +4,21 @@ import os
 import glob
 
 maxjobs = 3000
+first_job = 0
+# maxjobs = 3000
+first_job = 0
 jobcount = 0
+skipped_jobs = 0
+os.chdir('jobscripts_2016-07-18b')
 for job in glob.glob("subtree*.sh"):
+    if first_job > skipped_jobs:
+        skipped_jobs += 1
+        continue
     jobcount += 1
     if jobcount > maxjobs:
         break
     # print (job)
-    cmd = "qsub -cwd -j y -o %s.log -b y -l short=true -l h_rt=1500 -l d_rt=450 -V ./%s" % (job, job)
+    cmd = "qsub -cwd -j y -b y -o %s.log -l h_rt=3600 -l d_rt=750 -V ./%s" % (job, job)
     print (cmd)
     os.system(cmd)
 
